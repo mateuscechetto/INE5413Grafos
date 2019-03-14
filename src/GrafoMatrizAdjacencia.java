40165e842e909892fd5c8b4efad133b8fad0291a
Mateus Cechetto <mateuscechetto@gmail.com>
2019-03-14 16:16:55 -0300

    grafo matriz adjacencia com contador grau sem metodo de remocao de vertice

---
+++ src/GrafoMatrizAdjacencia.java
@@ -0,0 +1,106 @@
+import java.util.Arrays;
+
+public class GrafoMatrizAdjacencia implements Grafo {
+
+	private int[][] matriz;
+	private int[] vertices;
+	private int nVertices;
+	
+	public GrafoMatrizAdjacencia(int[] vertices) {
+		//poderia ser criado ja com espaco reservado para novos vertices mas nao acredito que haja demanda
+		this.vertices = vertices;
+		this.nVertices = vertices.length;
+		this.matriz = new int[nVertices][nVertices + 1]; 
+	}
+	
+	public GrafoMatrizAdjacencia(int[] vertices, int[][] arestas) {
+		this.vertices = vertices;
+		this.nVertices = vertices.length;
+		this.matriz = new int[nVertices][nVertices + 1]; 
+		for(int i = 0; i < arestas.length; i++) {
+			this.matriz[arestas[i][0]][arestas[i][1]] = 1;
+			this.matriz[arestas[i][0]][nVertices]++;
+			this.matriz[arestas[i][1]][arestas[i][0]] = 1;
+			this.matriz[arestas[i][1]][nVertices]++;
+		}
+	}
+	
+	@Override
+	public void insereVertice(int vertice) {
+		if(vertices.length == nVertices) {
+			this.vertices = Arrays.copyOf(this.vertices, this.vertices.length * 2);
+			this.nVertices++;
+			int[][] novaMatriz = new int[matriz.length * 2][matriz.length * 2 + 1];
+			for(int l = 0; l < matriz.length; l++) {
+				for(int c = 0; c < matriz.length; c++) {
+					novaMatriz[l][c] = matriz[l][c];
+				}
+				novaMatriz[l][novaMatriz.length] = matriz[l][matriz[l].length - 1];
+			}
+			this.matriz = novaMatriz;
+			for(int x = 0; x < novaMatriz.length; x++) {
+				for(int y = 0; y < novaMatriz[x].length; y++) {
+					System.out.print(novaMatriz[x][y] + " ");
+				}
+				System.out.println();
+			}
+		} else {
+			this.vertices[nVertices] = vertice;
+			this.nVertices++;
+		}
+	}
+
+	@Override
+	public void insereAresta(int vertice1, int vertice2) {
+		this.matriz[vertice1][vertice2] = 1;
+		this.matriz[vertice1][this.matriz[0].length - 1]++;
+		this.matriz[vertice2][vertice1] = 1;
+		this.matriz[vertice2][this.matriz[0].length - 1]++;
+	}
+
+	@Override
+	public boolean ehAdjacente(int vertice1, int vertice2) {
+		return this.matriz[vertice1][vertice2] == 1;
+	}
+
+	@Override
+	public int getGrau(int vertice) {
+		return this.matriz[vertice][this.matriz[0].length - 1];
+	}
+
+	@Override
+	public void imprimeVizinhosDe(int vertice) {
+		int vizinhosRestantes = this.matriz[vertice][this.matriz[0].length - 1];
+		for(int i = 0; i < this.matriz[vertice].length - 1; i++) {
+			if(vizinhosRestantes < 1) break;
+			if(this.matriz[vertice][i] == 1) {
+				System.out.println(vertices[i]);
+				vizinhosRestantes--;
+			}
+		}
+		
+	}
+
+	@Override
+	public void removeVertice(int vertice) {
+		
+	}
+
+	@Override
+	public void removeAresta(int vertice1, int vertice2) {
+		this.matriz[vertice1][vertice2] = 0;
+		this.matriz[vertice1][this.matriz[0].length - 1]--;
+		this.matriz[vertice2][vertice1] = 0;
+		this.matriz[vertice2][this.matriz[0].length - 1]--;
+	}
+
+	@Override
+	public void print() {
+		for(int l = 0; l < this.nVertices; l++) {
+			for(int c = 0; c < this.nVertices; c++) {
+				System.out.print(matriz[l][c] + " ");
+			}
+			System.out.println();
+		}
+	}
+}
--- src/Test.java
+++ src/Test.java
@@ -3,44 +3,46 @@ public class Test {
 
 	public static void main(String[] args) {
 		int[] vertices = {
+				0,
 				1,
 				2,
-				3,
 		};
 		int[][] arestas = {
+				{0,1},
 				{1,2},
-				{2,3},
 				};
-		GrafoHashMap g = new GrafoHashMap(vertices, arestas);
+		Grafo g = new GrafoMatrizAdjacencia(vertices, arestas);
 		g.print();
 		
-		g.insereVertice(4);
-		System.out.println("insere vertice 4");
+		g.insereVertice(3);
+		System.out.println("insere vertice 3");
 		g.print();
-		g.insereAresta(1, 4);
-		System.out.println("insere aresta {1,4}");
+		g.insereAresta(0, 3);
+		System.out.println("insere aresta {0,3}");
 		g.print();
-		System.out.print("1 adjacente a 2?  ");
-		System.out.println(g.ehAdjacente(1, 2));
-		System.out.print("2 adjacente a 1?  ");
-		System.out.println(g.ehAdjacente(2, 1));
-		System.out.print("4 adjacente a 3?  ");
-		System.out.println(g.ehAdjacente(4, 3));
-		System.out.println("vizinhos do 2");
-		g.imprimeVizinhosDe(2);
+		System.out.print("0 adjacente a 1?  ");
+		System.out.println(g.ehAdjacente(0, 1));
+		System.out.print("1 adjacente a 0?  ");
+		System.out.println(g.ehAdjacente(1, 0));
+		System.out.print("3 adjacente a 2?  ");
+		System.out.println(g.ehAdjacente(3, 2));
+		System.out.println("vizinhos do 1");
+		g.imprimeVizinhosDe(1);
 		for(int vertice: vertices) {
 			System.out.println("grau do vertice " + vertice + ": " + g.getGrau(vertice));
 		}
-		System.out.println("grau do vertice " + 4 + ": " + g.getGrau(4));
+		System.out.println("grau do vertice " + 3 + ": " + g.getGrau(3));
 
 		
-		g.removeVertice(2);
-		System.out.println("remove vertice 2");
+		g.removeVertice(1);
+		System.out.println("remove vertice 1");
 		g.print();
 		
-		g.removeAresta(1, 4);
-		System.out.println("remove aresta {1,4}");
+		g.removeAresta(0, 3);
+		System.out.println("remove aresta {0,3}");
 		g.print();
+		
+		
 		
 	}
 }
